10fd2cc85958723d15620ed2160d273386472f8c
AidanBugeja2 <147142113+AidanBugeja2@users.noreply.github.com>
2024-02-04 11:27:21 +0100

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,21 +1,34 @@
 import sqlite3
+
 def create_database():
+    """
+    Create a SQLite database file.
+    """
+    db_file = 'AidanBugeja.db'
+    conn = sqlite3.connect(db_file)
+    conn.close()
+
+def create_table():
+    """
+    Create a 'Routers' table in the SQLite database.
+    """
     db_file = 'AidanBugeja.db'
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    conn.commit()
-    conn.close()
-
-def create_table(self):
-    self.cursor.execute('''
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS Routers (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
             ip_address TEXT UNIQUE NOT NULL,
-            user TEXT NOT NULL,                password TEXT NOT NULL
-            )
-        ''')
-    self.conn.commit()
+            user TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
+# Create a SQLite database file
 create_database()
+
+# Create a 'Routers' table in the SQLite database
 create_table()
